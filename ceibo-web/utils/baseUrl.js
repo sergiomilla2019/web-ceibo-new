47ad7dc3ec79473bdf8b57b59c5eb41057a9ccad
Sergio Milla <sergiomilla2019@gmail.com>
2023-03-07 15:58:16 -0300

    Fix build

--- ceibo-web/utils/baseUrl.js
+++ ceibo-web/utils/baseUrl.js
@@ -1,7 +1,8 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://001-ceibo-web-front-bc71.vercel.app"
+    ? "https://001-ceibo-web-front-bc71.vercel.app" 
     : // ? "https://ceibo.digital"
       "http://localhost:3000"
 
+      
 export default baseUrl
