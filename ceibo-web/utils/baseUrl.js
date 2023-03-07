a2e3b7569adfbbd75bdc8abca6434444a2e64379
Sergio Milla <sergiomilla2019@gmail.com>
2023-03-07 16:25:41 -0300

    Add .env

--- ceibo-web/utils/baseUrl.js
+++ ceibo-web/utils/baseUrl.js
@@ -1,8 +1,8 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://001-ceibo-web-front-bc71.vercel.app" 
-    : // ? "https://ceibo.digital"
+    : // ? "https://ceibo.digital "
       "http://localhost:3000"
 
-      
+
 export default baseUrl
